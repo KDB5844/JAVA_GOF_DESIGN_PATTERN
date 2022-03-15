67cd9d279f3b6ccfb68bc5f8b94774f2a7c6353a
Subin Cho <sbjo7713@gmail.com>
2022-03-15 09:23:57 +0900

    Singleton패턴 synchronized, eager initialization, double checked locking, static inner 방식

--- src/main/java/me/whiteship/designpatterns/singleton/Settings.java
+++ src/main/java/me/whiteship/designpatterns/singleton/Settings.java
@@ -1,17 +1,70 @@
 package me.whiteship.designpatterns.singleton;
 
-public class Settings {
+import java.util.Set;
 
+public class Settings{
+
+    // private로 new 생성자 막기
+    private Settings(){};
+
+    /**
+     * synchronized 키워드 사용
+     * 대신 동기화 메커니즘으로 인해 약간의 성능저하 발생 -> 메서드 사용 중일 시 Lock
+     */
+    /*
     private static Settings instance;
-    //new 생성자 못하게 막기 위함
-    private Settings() {}
 
-    public static Settings getInstance() {
+    public static synchronized Settings getInstance() {
         if (instance == null) {
             instance = new Settings();
         }
+        return instance;
+    }*/
 
+
+
+    /**
+     * 이른 초기화 방법(eager initialization) 사용
+     * 미리 초기화를 하여 동일한 instance return
+     */
+    /*
+
+    private static final Settings INSTANCE = new Settings();
+
+    public static Settings getInstance() {
+        return INSTANCE;
+    }
+     */
+
+    /**
+     * double checked locking 사용하기
+     * instance를 필요한 시점에 만들 수 있음.
+     * instance가 없을 때 여러 thread가 동시에 if문을 통과 할 때만 synchronized
+     */
+
+    /*
+    private static volatile Settings instance;  //volatile keyword 사용해야 함.
+
+    public static Settings getInstance() {
+        if (instance == null) {
+            synchronized (Settings.class) {
+                if (instance == null) {
+                    instance = new Settings();
+                }
+            }
+        }
         return instance;
     }
+     */
 
+    /**
+     * static inner 클래스 사용
+     */
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
+
+    public static Settings getInstance() {
+        return SettingsHolder.INSTANCE;
+    }
 }
